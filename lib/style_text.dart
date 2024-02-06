4578101cf80aa0ecdd2a253b6781716a2df0bd8a
Kshitij-Anand7954 <kshitijkumar2002@gmail.com>
2024-02-06 12:32:08 +0530

    Initial Commit

---
+++ lib/style_text.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class StylingText extends StatelessWidget{
+ const StylingText({super.key});
+ @override
+ Widget build(context){
+  return const Text(
+              'Hello Kshitij !',style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 50,
+              ),
+              );
+
+ }
+  
+}
